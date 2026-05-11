bca2d63614b7c6e3af0c50aa6db3314ba97495ff
kevin-dbmi <kjdelaney@ucsd.edu>
2026-05-11 13:55:35 -0700

    Adding faculty list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,24 @@
 from datetime import datetime
-
+import logging
 import streamlit
 
+from src.dfm_research_paper_digest.faculty import Faculty
 from src.dfm_research_paper_digest.query_faculty_batch import run_batch_report
+from src.dfm_research_paper_digest.my_logging import setup_logging
 
-streamlit.title("Department of Family Medicine")
-streamlit.header("Publications Report")
+streamlit.title("UCSD Health")
+streamlit.header(" Department of Family Medicine")
+streamlit.subheader("Publications Report")
+
+log: logging.Logger = setup_logging()
+faculty: Faculty = Faculty("https://familymedicine.ucsd.edu/about/faculty.html", log)
+faculty_names: list[str] = faculty.names
+faculty_names.insert(0, "All")
+selection: streamlit.selectbox = streamlit.selectbox(
+    "Select faculty members for report.", faculty_names
+)
+streamlit.write(f"Selection: {selection}")
+
 progress_text: str = "Pulling data from PubMed..."
 my_bar: streamlit.progress = streamlit.progress(0, progress_text)
 
