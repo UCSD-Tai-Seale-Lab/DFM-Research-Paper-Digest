1e2dcc787e1717972d83475a5324322ff417c081
kevin-dbmi <kjdelaney@ucsd.edu>
2026-05-11 14:07:56 -0700

    Add UCSDH logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 import logging
 import streamlit
-
+from PIL import Image
 from src.dfm_research_paper_digest.faculty import Faculty
 from src.dfm_research_paper_digest.query_faculty_batch import run_batch_report
-from src.dfm_research_paper_digest.my_logging import setup_logging
+
+# from src.dfm_research_paper_digest.my_logging import setup_logging
 
 streamlit.title("UCSD Health")
+image: Image = Image.open("logos-clinicalHealth-full.png")
+streamlit.image(image)
 streamlit.header(" Department of Family Medicine")
 streamlit.subheader("Publications Report")
 
-log: logging.Logger = setup_logging()
-faculty: Faculty = Faculty("https://familymedicine.ucsd.edu/about/faculty.html", log)
+# log: logging.Logger = setup_logging()
+faculty: Faculty = Faculty("https://familymedicine.ucsd.edu/about/faculty.html")
 faculty_names: list[str] = faculty.names
 faculty_names.insert(0, "All")
 streamlit.write(f"Received {len(faculty_names)} names.")
